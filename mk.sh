d7317612f4f622a92330712f9290d48d99f0216f
descent <descent@weiya.com.tw>
2013-03-05 15:08:17 +0800

    refine it

--- mk.sh
+++ mk.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
-zip -r foo . -i "/*"
+CUR_DIR=`basename $PWD`.epub
+if [ "$1" != "" ] ; then
+  CUR_DIR=$1
+fi
 
+echo "create epub: $CUR_DIR"
+
+zip -r foo . -i "/*"
+mv foo.zip $CUR_DIR
+ls -l $CUR_DIR
